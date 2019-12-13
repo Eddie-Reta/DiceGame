ca0ef55be8ce745f47774dc0fe3efefa298bb2cd
eduardo reta rj <https://github.com/Eddie-Reta>
2019-12-13 18:39:50 -0500

    current player dice rolls are added next store the score to the player

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,15 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-//creates random dice number roll 1 - 6
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    //1. Random number
+    //creates random dice number roll 1 - 6
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM =  document.querySelector('.dice');
+
+        diceDOM.style.display = 'block';
+    
+    //2. Display the score
+        diceDOM.src = './assets/images/dice-' + dice + '.png';
+        
+    //3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+        } else {
+            //next player
+            //if active player equals 0  then active player should be 1 else active player should be 0;
+  
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            roundScore = 0;
+            document.getElementById('current-0').textContent = '0';
+            document.getElementById('current-1').textContent = '0';
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+            document.querySelector('.dice').style.display = 'none';
+        }
+});
